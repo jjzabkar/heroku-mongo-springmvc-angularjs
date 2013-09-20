cad32c5f9473124c9c1059293b3104b7fed18dfa
jjzabkar <jjzabkar@gmail.com>
2013-09-20 11:10:03 -0700

    Added

---
+++ src/main/java/com/jjz/config/GitRepositoryState.java
@@ -0,0 +1,46 @@
+package com.jjz.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * From <a href="https://github.com/ktoso/maven-git-commit-id-plugin/tree/master">maven-git-commit-id-plugin</a>.
+ */
+public class GitRepositoryState {
+
+	@Value("${git.branch}")
+	public String branch;
+
+	@Value("${git.commit.id.describe}")
+	public String describe;
+
+	@Value("${git.commit.id}")
+	public String commitId;
+
+	@Value("${git.commit.id.abbrev}")
+	public String commitIdAbbrev;
+
+	@Value("${git.build.user.name}")
+	public String buildUserName;
+
+	@Value("${git.build.user.email}")
+	public String buildUserEmail;
+
+	@Value("${git.build.time}")
+	public String buildTime;
+
+	@Value("${git.commit.user.name}")
+	public String commitUserName;
+
+	@Value("${git.commit.user.email}")
+	public String commitUserEmail;
+
+	@Value("${git.commit.message.full}")
+	public String commitMessageFull;
+
+	@Value("${git.commit.message.short}")
+	public String commitMessageShort;
+
+	@Value("${git.commit.time}")
+	public String commitTime;
+
+}
