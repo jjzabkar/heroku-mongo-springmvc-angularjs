7f87dde2af7e2a7eeafd7d854d6492a1b4d832e4
jjzabkar <jjzabkar@gmail.com>
2013-09-20 11:09:20 -0700

    Updated

--- src/main/java/com/jjz/config/WebModuleConfiguration.java
+++ src/main/java/com/jjz/config/WebModuleConfiguration.java
@@ -2,9 +2,13 @@ package com.jjz.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Controller;
  */
 @Configuration
 @Import({ MongoDbConfiguration.class })
+@PropertySource("classpath:git.properties")
 @ComponentScan(basePackages = { "com.jjz" }, excludeFilters = @ComponentScan.Filter({ Controller.class, Configuration.class }))
 public class WebModuleConfiguration {
 
@@ -21,6 +26,17 @@ public class WebModuleConfiguration {
 
 	public WebModuleConfiguration() {
 		logger.info("init");
+	}
+
+	@Bean(name = "propertySourcesPlaceholderConfigurer")
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean
+	@DependsOn("propertySourcesPlaceholderConfigurer")
+	public GitRepositoryState gitRepositoryState() {
+		return new GitRepositoryState();
 	}
 
 }
